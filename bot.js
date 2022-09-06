4da4a850228fd063994341fcf3700caf7d17a666
maki-engineer <ryohei-81@outlook.jp>
2022-09-06 12:40:26 +0900

    update: SQLite導入

--- bot.js
+++ bot.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// SQLite3 導入
+const sqlite3 = require("sqlite3");
+const db      = new sqlite3.Database("235data.db");
+
 const { Client, GatewayIntentBits } = require("discord.js");
 const token                         = require("./discord-token.json");
 const client                        = new Client({
