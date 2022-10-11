b4d5f8352a2995dc1b92a54263b5b71274cec45f
maki-engineer <ryohei-81@outlook.jp>
2022-10-11 19:26:56 +0900

    fix: スコープを意識して、被りチェックの機能を修正

--- bot.js
+++ bot.js
@@ -2056,6 +2056,7 @@ client.on("messageCreate", (message) => {
 
   }else if(command === "roomdivision"){  // roomdivisionコマンド ボイスチャンネルに参加しているメンバーを2つに分ける
 
+    // 雑談チャンネルに参加しているメンバー一覧をシャッフル
     let members     = client.voice.client.channels.cache.get(information.voice_channel_for_235_chat_place).members.map(member => member.user);
     members         = def.shuffle(members);
     let membersName = members.map(name => name.username);
@@ -2075,6 +2076,8 @@ client.on("messageCreate", (message) => {
 
       }else{
 
+        message.channel.sendTyping();
+
         let divisionCount    = 0;
         let duplicationCount = 100;
         let halfMembersName1 = [];
@@ -2084,89 +2087,88 @@ client.on("messageCreate", (message) => {
         let halfIndex1       = 0;
         let halfIndex2       = 0;
 
-        // この処理が失敗したら2,079行目と2,088～2,169行目を削除し、コメントアウトしている処理に直すこと
-        while(duplicationCount >= 3){
-          duplicationCount = 0;
 
-          if(membersName.length % 2 === 0){
+        // この処理が失敗したら2,081行目と2,092～2,170行目を削除し、コメントアウトしている処理に直すこと
+        db.all("select * from half_members", (err, rows) => {
+          let dataIds = rows.map(data => data.id);
+          while(duplicationCount >= 3){
+            // 初期化
+            duplicationCount = 0;
 
-            halfIndex1 = Math.floor(membersName.length / 2) - 1;
-            halfIndex2 = membersName.length - halfIndex1 - 1;
+            // 配列を2個の配列に分ける
+            if(membersName.length % 2 === 0){
 
-          }else{
+              halfIndex1 = Math.floor(membersName.length / 2) - 1;
+              halfIndex2 = membersName.length - halfIndex1 - 1;
 
-            halfIndex1  = Math.floor(membersName.length / 2);
-            halfIndex2  = membersName.length - halfIndex1;
+            }else{
 
-          }
-  
-          for(let i = 0; i <= halfIndex1; i++){
+              halfIndex1  = Math.floor(membersName.length / 2);
+              halfIndex2  = membersName.length - halfIndex1;
 
-            halfMembersName1.push(membersName[i]);
-            halfMembersId1.push(membersId[i]);
-
-          }
-  
-          for(let i = halfIndex2; i < membersName.length; i++){
-
-            halfMembersName2.push(membersName[i]);
-            halfMembersId2.push(membersId[i]);
-
-          }
-
-          db.all("select * from half_members", (err, rows) => {
-            for(let oi = 0; oi < halfMembersId2.length; oi++){
-              for(let ii = 0; ii < rows.length; ii++){
-                if(halfMembersId2[oi] === rows[ii].id){
-
-                  count++;
-                  break;
-
-                }
-              }
             }
-          });
 
-          if(count < 3){
+            for(let i = 0; i <= halfIndex1; i++){
 
-            db.run("delete from half_members");
+              halfMembersName1.push(membersName[i]);
+              halfMembersId1.push(membersId[i]);
 
-            message.channel.sendTyping();
-            setTimeout(() => message.reply("このような結果になりました！\n\n**雑談**\n------------------------------------------------------------\n" + halfMembersName1.join("\n") + "\n------------------------------------------------------------\n\n**雑談その2**\n------------------------------------------------------------\n" + halfMembersName2.join("\n") + "\n------------------------------------------------------------\n\n自動で分けられますのでしばらくお待ちください。"), 2_000);
+            }
 
-            setTimeout(() => {
+            for(let i = halfIndex2; i < membersName.length; i++){
 
-              let roomDivisionTimer = setInterval(() => {
-                if(divisionCount === halfMembersName2.length){
+              halfMembersName2.push(membersName[i]);
+              halfMembersId2.push(membersId[i]);
 
-                  message.delete()
-                  .then((data) => data)
-                  .catch((err) => err);
-                  clearInterval(roomDivisionTimer);
+            }
 
-                }else{
+            // 3人以上被ってないかチェック
+            count = halfIds2.filter(x => dataIds.indexOf(x) !== -1).length;
 
-                  db.run("insert into half_members(id) values(?)", halfMembersId2[divisionCount]);
-                  client.guilds.cache.get(information.server_for_235).members.fetch(halfMembersId2[divisionCount]).then((user) => user.voice.setChannel(information.voice_channel_for_235_chat_place_2));
-                  divisionCount++;
+            // 2個目の配列の人達を雑談その2に移動させる
+            if(count < 3){
 
-                }
-              }, 1_000);
+              db.run("delete from half_members");
 
-            }, 9_000);
+              setTimeout(() => message.reply("このような結果になりました！\n\n**雑談**\n------------------------------------------------------------\n" + halfMembersName1.join("\n") + "\n------------------------------------------------------------\n\n**雑談その2**\n------------------------------------------------------------\n" + halfMembersName2.join("\n") + "\n------------------------------------------------------------\n\n自動で分けられますのでしばらくお待ちください。"), 2_000);
 
-            break;
+              setTimeout(() => {
+
+                let roomDivisionTimer = setInterval(() => {
+                  if(divisionCount === halfMembersName2.length){
+
+                    message.delete()
+                    .then((data) => data)
+                    .catch((err) => err);
+                    clearInterval(roomDivisionTimer);
+
+                  }else{
+
+                    db.run("insert into half_members(id) values(?)", halfMembersId2[divisionCount]);
+                    client.guilds.cache.get(information.server_for_235).members.fetch(halfMembersId2[divisionCount]).then((user) => user.voice.setChannel(information.voice_channel_for_235_chat_place_2));
+                    divisionCount++;
+
+                  }
+                }, 1_000);
+
+              }, 9_000);
+
+              break;
+
+            }
+
+            // 初期化
+            members          = def.shuffle(members);
+            membersName      = members.map(name => name.username);
+            membersId        = members.map(id => id.id);
+            halfMembersName1 = [];
+            halfMembersName2 = [];
+            halfMembersId1   = [];
+            halfMembersId2   = [];
 
           }
+        });
 
-          members          = def.shuffle(members);
-          membersName      = members.map(name => name.username);
-          membersId        = members.map(id => id.id);
-          halfMembersName1 = [];
-          halfMembersName2 = [];
-          halfMembersId1   = [];
-          halfMembersId2   = [];
-        }
 
         /*
         if(membersName.length % 2 === 0){
