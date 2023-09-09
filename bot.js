f4d00156d6e24850cb457778fc0921f56989a770
maki-engineer <ryohei-81@outlook.jp>
2023-09-09 20:10:00 +0900

    update: if文の間にスペースを追加

--- bot.js
+++ bot.js
@@ -1357,7 +1357,7 @@ client.on("messageCreate", message => {
         }
       });
     }
-  }else if(command === "help"){          // helpコマンド 235botの機能一覧を教える。
+  } else if (command === "help") {          // helpコマンド 235botの機能一覧を教える。
     switch (message.author.id) {
       case information.user_for_utatane:
         message.reply("235botは以下のようなコマンドを使用することが出来ます。\n\n・235ap\n\n・235apremove\n\n・235apall\n\n・235notap\n\n・235apsearch\n\n・235birthday\n\n・235men\n\n・235roomdivision\n\n各コマンドの機能の詳細を知りたい場合は、スラッシュコマンド **/** を使って知りたい機能を選択してください。");
